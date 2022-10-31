052299c67078d0ef82fc9c2f83628bb7b66ac6db
Krithika <krtkarvkmr@gmail.com>
2022-10-31 22:55:39 +0530

    Second commit

--- src/main/java/com/krithika/mavenproject/App.java
+++ src/main/java/com/krithika/mavenproject/App.java
@@ -4,6 +4,9 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Maven Krihtika");
+		
+		Dummy d = new Dummy();
+		d.print();
 
 	}
 
---
+++ src/main/java/com/krithika/mavenproject/Dummy.java
@@ -0,0 +1,12 @@
+package com.krithika.mavenproject;
+
+public class Dummy {
+	
+	public void print()
+	{
+		System.out.println("hello class2");
+	}
+	
+	
+
+}
